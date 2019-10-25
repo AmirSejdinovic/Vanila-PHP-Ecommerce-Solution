4ed6ee5e647d895d7ab6aad58f0c02d786057bef
Amir Sejdinovic <amir24V@yahoo.com>
2019-10-25 13:49:23 +0200

    Displaying categoreis from database

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -1,8 +1,29 @@
 <div class="col-md-3">
                 <p class="lead">Shop Name</p>
                 <div class="list-group">
-                    <a href="category.html" class="list-group-item">Category 1</a>
-                    <a href="#" class="list-group-item">Category 2</a>
-                    <a href="#" class="list-group-item">Category 3</a>
+               <?php
+
+                //Here I created the query which selects all from categories table
+                 $query = "SELECT * FROM  categories";
+                 //Here I send query into db 
+                 $send_query = mysqli_query($connection, $query);
+                //This if statement cheks if has error in query and if does it die all the code and display the errors
+                 if(!$send_query){
+                    die("QUERY FAILED" . mysqli_error($connection));
+                 }
+                  //This is the while loop with it I fatch the data from database and display ti
+                 while($row = mysqli_fetch_array($send_query)){
+
+
+                  echo "<a href='category.html' class='list-group-item'>{$row['cat_title']}</a>";
+
+                 }
+
+
+                   ?>
+
+             
+
+                    
                 </div>
             </div>
