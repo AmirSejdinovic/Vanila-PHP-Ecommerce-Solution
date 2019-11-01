7efc12c7f2e1ae0990d8599798a6e145bf1d94b3
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-01 14:04:46 +0100

    Building tahank you page and inserting paypal return data into db

--- public/thank_you.php
+++ public/thank_you.php
@@ -9,9 +9,12 @@
       $currency = $_GET['cc'];
       $transaction = $_GET['tx'];
       $status = $_GET['st'];
-
-      
-
+     //Inserting the pay pal return parametars into database
+      $query = query("INSERT INTO orders (order_amount, order_transaction, 	order_status, order_currency) VALUES('{$amount}', '{$transaction}', '{$status}', '{$currency}')");
+      //Testing query
+      confirm($query);
+      //destroying the session for reset 
+      session_destroy();
       
      }else{
        //if the request do not have the get with kex of tx than redirect to index.php
