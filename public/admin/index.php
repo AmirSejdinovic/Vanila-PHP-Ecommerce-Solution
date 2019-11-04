f8b6e307496f17961ff65545299d79339fb274b0
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-04 14:13:45 +0100

    Refactoring admin template

--- public/admin/index.php
+++ public/admin/index.php
@@ -30,9 +30,25 @@ include(TEMPLATE_BACK . DS . "header.php");
                     include(TEMPLATE_BACK . DS . "admin_content.php");
                  }
 
-
+                 //If the url have the get request with key 'orders' than include orders.php page
                 if(isset($_GET['orders'])){
                     include(TEMPLATE_BACK . DS . "orders.php");
+                }
+                //If the url have the get request with key 'add_product' than include add_product.php page
+                if(isset($_GET['add_product'])){
+                    include(TEMPLATE_BACK . DS . "add_product.php");
+                }
+                //If the url have the get request with key 'edit_product' than include edit_product.php page
+                if(isset($_GET['edit_product'])){
+                    include(TEMPLATE_BACK . DS . 'edit_product.php');
+                }
+                //If the url have the get request with kex 'categores' than include the categories.php page
+                if(isset($_GET['categories'])){
+                    include(TEMPLATE_BACK . DS . 'categories.php');
+                }
+               //If the url have the get request with key of 'products' than include products.php page
+                if(isset($_GET['products'])){
+                    include(TEMPLATE_BACK . DS . 'products.php');
                 }
 
 
