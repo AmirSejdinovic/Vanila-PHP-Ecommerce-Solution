871e1778f4d6e8398d53c15cad79118625143ed4
Amir Sejdinovic <amir24V@yahoo.com>
2019-11-13 12:58:27 +0100

    Deleting slider images from folder

--- resources/templates/back/delete_slide.php
+++ resources/templates/back/delete_slide.php
@@ -3,10 +3,27 @@
 
 //If get request have the key id than do the code below
 if(isset($_GET['delete_slide_id'])){
+
+  //Selecting the slide image in database and detect the that image vie slide_id
+  $going_insade_db_for_image_name = query("SELECT slide_image FROM slides WHERE slide_id = " . escape_string($_GET['delete_slide_id']) ."LIMIT 1");
+  //testing query
+  confirm($going_insade_db_for_image_name);
+  //Fetching the result
+  $row = fetch_array($going_insade_db_for_image_name);
+  //Declaring the path to uploads folder
+  $target_path = "../../resources/uploads/{$row['slide_image']}";
+  //Deleting the image from foleder
+  unlink($target_path);
    //Creating query and pasing it to database. With this query I delete order from orders table which has the id from get requiest
   $query = query("DELETE FROM slides WHERE slide_id = ".escape_string($_GET['delete_slide_id']) . "");
   //testing
   confirm($query);
+
+  
+
+  
+  
+ 
   //Set mesage fro dislay in front end
   set_message("Slide has been deleted");
   //redirecting back to orders page
